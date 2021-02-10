ef95b0786c611289f9a3e8f256ecdcacf5bdc705
NathanQSilva <72088284+NathanQSilva@users.noreply.github.com>
2021-02-10 02:23:19 -0300

    teste com template e variaveis adicionado

--- Projeto/Blog/website/views.py
+++ Projeto/Blog/website/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def hello_blog(request):
+    lista = ['nathan', 'marco', 'joao', 'carol', 'carla']
+    data = {'name': 'Curso de django 3', 'lista_tec': lista}
+    return render(request, 'index.html', data)
